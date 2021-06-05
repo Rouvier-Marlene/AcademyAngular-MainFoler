8b9d3a2060f3c6ee542bc2bdf51348d52d29a790
Marlene Rouvier <marlene.rouvier@globant.com>
2021-06-05 20:03:59 -0300

    "Assignment 6 - Completed!"

--- Section15/Assignment6/src/app/app.component.spec.ts
+++ Section15/Assignment6/src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'Assignment6'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Assignment6');
   });
 
   it('should render title', () => {
