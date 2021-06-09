48bdfac5daacabb7b200459eea885af55e1c361f
Marlene Rouvier <marlene.rouvier@globant.com>
2021-06-09 15:15:33 -0300

    "SourseProject-Http - Starting code"

--- Section19/CourseProject-HttpRequests/src/app/app.module.ts
+++ Section19/CourseProject-HttpRequests/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -37,7 +36,6 @@ import { RecipeService } from './recipes/recipe.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService, RecipeService],
