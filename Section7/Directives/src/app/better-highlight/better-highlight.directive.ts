b9c783c51ec83e84eaf9c864eb4aae8e82851e01
Marlene Rouvier <marlene.rouvier@globant.com>
2021-06-09 22:06:31 -0300

    "Practice Directives - Completed!"

---
+++ Section7/Directives/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,37 @@
+import { 
+  Directive, 
+  ElementRef, 
+  HostBinding, 
+  HostListener, 
+  Input, 
+  OnInit, 
+  Renderer2 
+} from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'lightgreen';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+    
+  constructor(private elementRef: ElementRef,
+              private renderer: Renderer2) { }
+
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'lightgreen', false, false);
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'lightgreen');
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'transparent');
+    this.backgroundColor = this.defaultColor;
+  }
+
+}
