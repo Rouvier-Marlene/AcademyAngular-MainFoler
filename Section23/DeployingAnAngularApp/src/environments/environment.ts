3771e7758f3f4c99f92b8c4d1c01d297a741a781
Marlene Rouvier <marlene.rouvier@globant.com>
2021-06-14 19:02:51 -0300

    "Deploying an Angular App - Completed!"

--- Section23/DeployingAnAngularApp/src/environments/environment.ts
+++ Section23/DeployingAnAngularApp/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyBGhFbSM9z3zEelwV9i8kCzsR3VOG9nVCE'
 };
 
 /*
